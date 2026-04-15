5e1048f787a0341982a19c6f9dfb7e26fec70c5f
Valentina kricinski kuelkamp <valentina.kuelkamp@gmail.com>
2026-04-15 15:26:11 -0300

    Update Uni4Exe27.java

--- src/Uni4/Uni4Exe27.java
+++ src/Uni4/Uni4Exe27.java
@@ -2,7 +2,7 @@ package Uni4;
 
 import java.util.Scanner;
 
-public class Estacionamento {
+public class Uni4Exe27 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
