842c10c50703a1af970cb4c38c06b3806e83cda9
Valentina kricinski kuelkamp <valentina.kuelkamp@gmail.com>
2026-04-15 15:17:26 -0300

    Update Uni4Exe24.java

--- src/Uni4/Uni4Exe24.java
+++ src/Uni4/Uni4Exe24.java
@@ -1,74 +1,69 @@
-package Uni4;
+/* Dados 3 valores, escreva um algoritmo que os informe em uma determinada 
+ordem a partir de um menu de opções:
+
+se opção = 1, escreva os 3 valores em ordem crescente
+se opção = 2, escreva os 3 valores em ordem decrescente
+se opção = 3, escreva os 3 valores de forma que o maior valor fique no meio
+
+Exemplos de entrada	Exemplos de saída
+Valor 1: 5 Valor 2: 3 Valor 3: 2 Opção: 1	2 3 5
+Valor 1: 5 Valor 2: 3 Valor 3: 2 Opção: 2	5 3 2
+Valor 1: 5 Valor 2: 3 Valor 3: 2 Opção: 3	3 5 2 */
 
 import java.util.Scanner;
 
-public class Uni4Exe24 {
+public class Prova24 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
-        System.out.print("insira o valor 1: ");
+        System.out.println("v1:");
         int v1 = sc.nextInt();
-
-        System.out.print("insira o valor 2: ");
+        System.out.println("v2:");
         int v2 = sc.nextInt();
-
-        System.out.print("insira o valor 3: ");
+        System.out.println("v3:");
         int v3 = sc.nextInt();
-
-        System.out.print("insira a opção: ");
+        System.out.println("opcao:");
         int opcao = sc.nextInt();
 
-        int menor, meio, maior;
-
-        if (v1 <= v2 && v1 <= v3) {
-            menor = v1;
-            if (v2 <= v3) {
-                meio = v2;
-                maior = v3;
-            } else {
-                meio = v3;
-                maior = v2;
-            }
-        } else if (v2 <= v1 && v2 <= v3) {
-            menor = v2;
-            if (v1 <= v3) {
-                meio = v1;
-                maior = v3;
-            } else {
-                meio = v3;
-                maior = v1;
-            }
-        } else {
-            menor = v3;
-            if (v1 <= v2) {
-                meio = v1;
-                maior = v2;
-            } else {
-                meio = v2;
-                maior = v1;
-            }
+        int maior,menor,meio;
+        /*maior */
+        if(v1>=v2 && v1>=v3){
+            maior=v1;
+        }else if(v2>=v1 && v2>=v3){
+            maior=v2;
+        }else{
+            maior=v3;
         }
+
+        /*menor */
+        if(v1<=v2 && v1<=v3){
+            menor=v1;
+        }else if(v2<=v1 && v2<=v3){
+            menor=v2;
+        }else{
+            menor=v3;
+        }
+
+        /*meio */
+        meio= v1+v2+v3-menor-maior;
 
         switch (opcao) {
             case 1:
-
                 System.out.println(menor + " " + meio + " " + maior);
                 break;
 
             case 2:
-
                 System.out.println(maior + " " + meio + " " + menor);
                 break;
 
             case 3:
-
-                System.out.println(menor + " " + maior + " " + meio);
+                System.out.println(meio + " " + maior + " " + menor);
                 break;
 
             default:
-                System.out.println("Opção inválida!");
+                System.out.println("Opcao invalida");
         }
 
         sc.close();
     }
+ 
 }
